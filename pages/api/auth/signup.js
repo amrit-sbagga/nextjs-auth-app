c2e90d69d74411b83e5a351ae70bf6683bc3eef4
Amrit Pal Singh <amrit.s@proximity.tech>
2022-01-22 20:49:35 +0530

    Sending signup requests from the frontend

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,26 +1,39 @@
 import { hashedPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
-async function handler(req, res){
-    const data = req.body;
-    const { email, password } = data;
+async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+  const data = req.body;
+  const { email, password } = data;
 
-    if(!email || !email.includes('@') || !password || password.trim().length < 7){
-        res.status(422).json({message : 'Invalid input - password should be atleast 7 characters long.'})
-        return;
-    }
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res
+      .status(422)
+      .json({
+        message:
+          "Invalid input - password should be atleast 7 characters long.",
+      });
+    return;
+  }
 
-    const client = await connectToDatabase();
+  const client = await connectToDatabase();
 
-    const db = client.db();
+  const db = client.db();
 
-    const hashPassword = hashedPassword(password);
-    const result = await db.collection('users').insertOne({
-        email : email,
-        password : hashPassword
-    });
+  const hashPassword = await hashedPassword(password);
+  const result = await db.collection("users").insertOne({
+    email: email,
+    password: hashPassword,
+  });
 
-    res.status(201).json({message : 'Created user!'})
+  res.status(201).json({ message: "Created user!" });
 }
 
 export default handler;
